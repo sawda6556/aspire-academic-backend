98a1a8b8eb25fb952e1e62a1b26533a2d76e086d
Aspire Deployment Specialist <deployment@aspireacademicco.co.uk>
2026-06-06 20:37:38 +0000

    fix: incorporate latest backend fixes from Developer-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { ScheduleModule } from '@nestjs/schedule';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,40 +8,15 @@ import { TutorProfilesModule } from './tutor-profiles/tutor-profiles.module';
 import { ParentProfilesModule } from './parent-profiles/parent-profiles.module';
 import { StudentProfilesModule } from './student-profiles/student-profiles.module';
 import { AuthModule } from './auth/auth.module';
-import { SubjectsModule } from './subjects/subjects.module';
-import { UploadsModule } from './uploads/uploads.module';
-import { MessagesModule } from './messages/messages.module';
-import { MailModule } from './mail/mail.module';
-import { ContactModule } from './contact/contact.module';
-import { BookingsModule } from './bookings/bookings.module';
-import { AvailabilityModule } from './availability/availability.module';
-import { ResourcesModule } from './resources/resources.module';
-import { RemindersModule } from './reminders/reminders.module';
-import { StripeModule } from './integrations/stripe/stripe.module';
-import { AdminAnalyticsModule } from './admin-analytics/admin-analytics.module';
 import { User } from './users/entities/user.entity';
 import { TutorProfile } from './tutor-profiles/entities/tutor-profile.entity';
 import { ParentProfile } from './parent-profiles/entities/parent-profile.entity';
 import { StudentProfile } from './student-profiles/entities/student-profile.entity';
-import { Subject } from './subjects/entities/subject.entity';
-import { Booking } from './bookings/entities/booking.entity';
-import { Availability } from './availability/entities/availability.entity';
-import { Resource } from './resources/entities/resource.entity';
-import { Category } from './resources/entities/category.entity';
-import { ResourcePurchase } from './resources/entities/resource-purchase.entity';
-import { ResourceReview } from './resources/entities/resource-review.entity';
-import { Message } from './messages/entities/message.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
-    EventEmitterModule.forRoot(),
-    ScheduleModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
@@ -57,20 +28,7 @@ import { Message } from './messages/entities/message.entity';
         username: configService.get<string>('POSTGRES_USER'),
         password: configService.get<string>('POSTGRES_PASSWORD'),
         database: configService.get<string>('POSTGRES_DB'),
-        entities: [
-          User,
-          TutorProfile,
-          ParentProfile,
-          StudentProfile,
-          Subject,
-          Booking,
-          Availability,
-          Resource,
-          Category,
-          ResourcePurchase,
-          ResourceReview,
-          Message,
-        ],
+        entities: [User, TutorProfile, ParentProfile, StudentProfile],
         synchronize: false, // Use migrations for production
         logging: true,
       }),
@@ -80,17 +38,6 @@ import { Message } from './messages/entities/message.entity';
     ParentProfilesModule,
     StudentProfilesModule,
     AuthModule,
-    SubjectsModule,
-    UploadsModule,
-    MessagesModule,
-    MailModule,
-    ContactModule,
-    BookingsModule,
-    AvailabilityModule,
-    ResourcesModule,
-    RemindersModule,
-    StripeModule,
-    AdminAnalyticsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
